45d1ed700abb8638e4e5e9a4537a8cee8d6f4907
Antonio Soto <antonio.soto@xing.com>
2023-03-22 16:38:38 +0100

    feat: add Hiberus

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 describe('App', () => {
   it('renders headline', () => {
     render(<App />);
-    expect(screen.getByText('Vite + React + Fly.io')).toBeTruthy()
+    expect(screen.getByText('Vite + React + Fly.io + Hiberus')).toBeTruthy()
   });
 });
